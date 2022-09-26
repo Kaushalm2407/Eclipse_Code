023816cfe65925a921af1da118f5350e0173790a
Kaushal Mishra <kaushalm797@gmail.com>
2022-09-27 00:34:56 +0530

    commit -Java

---
+++ Java/src/Operators.java
@@ -0,0 +1,31 @@
+
+public class Operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		Variables
+		int a=10;
+		int b=20;
+		
+//		Arithmetic Operators 
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		
+//		Relational Operators
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a>=b);
+		System.out.println(a<=b);
+		System.out.println(a!=b);
+		System.out.println(a==b);
+		
+//		Logical Operators
+		System.out.println(a&b);
+		
+	}
+
+}
